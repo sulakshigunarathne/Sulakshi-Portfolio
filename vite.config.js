02baedb2328fc9c86fda342351e94aabcab42e69
sulakshi <gunarathneawsm.20@uom.lk>
2025-01-20 10:56:08 +0530

    gitignore added

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import glsl from 'vite-plugin-glsl'
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/Sulakshi-Portfolio",
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Sulakshi-Portfolio/",
-  plugins: [react(), glsl()],
+  plugins: [react()],
 })
